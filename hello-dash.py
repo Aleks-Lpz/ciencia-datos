6842eb78add24f3fb5b4ede278b8b44b4c0d3f6e
Aleks <aleks.lpx@gmail.com>
2026-08-31 16:02:53 +0000

    lol2

--- hello-dash.py
+++ hello-dash.py
@@ -2,5 +2,5 @@ import streamlit as st
 import pandas as pd 
 st.title("hello world web")
 st.write("hello world streamlit")
-dataframe = pd.read_csv("https://raw.githubusercontent.com/adsoftsito/ciencia-datos/refs/heads/main/titanic.csv")
+dataframe = pd.read_csv("https://raw.githubusercontent.com/Aleks_Lpz/ciencia-datos/refs/heads/main/titanic.csv")
 st.dataframe(dataframe)
